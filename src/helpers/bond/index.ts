9e4947db7a02ff43cd8909a736056cc3a13b6061
DreamSky1996 <webhipe@gmail.com>
2021-12-02 02:51:18 +0900

    init deploy project

--- src/helpers/bond/index.ts
+++ src/helpers/bond/index.ts
@@ -18,7 +18,7 @@ export const mim = new StableBond({
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
         [Networks.AVAX]: {
-            bondAddress: "0x07df32EF3159c784a12E58dAf9d32eB236a30D6f",
+            bondAddress: "",
             reserveAddress: "0x130966628846BFd36ff31a822705796e8cb8C18D",
         },
     },
@@ -34,7 +34,7 @@ export const wavax = new CustomBond({
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
         [Networks.AVAX]: {
-            bondAddress: "0xfcbd66A7D4674Bba16764Ea413e7f070A8Aa1e2f",
+            bondAddress: "",
             reserveAddress: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
         },
     },
@@ -50,11 +50,11 @@ export const mimKandy = new LPBond({
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
         [Networks.AVAX]: {
-            bondAddress: "0xd0023548349FeD39e1CAB232D02766E40067ADE0",
-            reserveAddress: "0xf12B055625200a6e30c35424b9F02A76BCF9d106",
+            bondAddress: "",
+            reserveAddress: "",
         },
     },
-    lpUrl: "https://www.traderjoexyz.com/#/pool/0x130966628846BFd36ff31a822705796e8cb8C18D/0x3e02f97774da99fB897FB08bF16E63A1706C41Bd",
+    lpUrl: "https://www.traderjoexyz.com/#/pool/0x130966628846BFd36ff31a822705796e8cb8C18D/",
 });
 
 export const avaxKandy = new CustomLPBond({
@@ -66,11 +66,11 @@ export const avaxKandy = new CustomLPBond({
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
         [Networks.AVAX]: {
-            bondAddress: "0x46207878E76D903f830980A42Ec16716ae655369",
-            reserveAddress: "0xD20d23772324737A6FD376002DC8E0842bA4551a",
+            bondAddress: "",
+            reserveAddress: "",
         },
     },
-    lpUrl: "https://www.traderjoexyz.com/#/pool/AVAX/0x3e02f97774da99fB897FB08bF16E63A1706C41Bd",
+    lpUrl: "https://www.traderjoexyz.com/#/pool/AVAX/",
 });
 
 export default [mim, wavax, mimKandy, avaxKandy];
